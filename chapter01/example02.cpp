d7b0097862fc0ab484fd928a0cf40b8f7f294047
Ronald Theodoro <ronaldtheodoro@gmail.com>
2023-05-08 18:55:42 -0300

    initial commit

---
+++ chapter01/example02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int idade;
+    cout << "Digite a sua idade: ";
+    cin >> idade;
+    cout << "Você tem " << idade << " anos de idade" << "\n";
+    return 0;
+}
---
+++ chapter02/example03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int valor_dec = 15;
+    int valor_hex = 0xF;
+    int valor_oct = 017;
+    cout << "Valor decimal: " << valor_dec << endl;
+    cout << "Valor hexadecimal: " << valor_hex << endl;
+    cout << "Valor octal: " << valor_oct << endl;
+    return 0;
+}
